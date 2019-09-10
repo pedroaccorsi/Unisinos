a78ffa97be9260e6685e7c82dd7c92982931b7b5
pedroaccorsi <pedrooacorsi@gmail.com>
2019-09-10 20:33:40 -0300

    Update applications_menu.c

--- Computer-science/3rd_semester/operational_systems/laboratory/trabalho_ga/applications_menu.c
+++ Computer-science/3rd_semester/operational_systems/laboratory/trabalho_ga/applications_menu.c
@@ -1,19 +1,121 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <locale.h>
-#include "utilities.h"
 
-int main(){   
+//Definições de menu
+#define WEB_BROWSER         1
+#define TEXT_EDITOR         2
+#define TERMINAL            3
+#define FINALIZAR_PROCESSO  4
+#define QUIT                5
 
-    setlocale(LC_ALL, "Portuguese");
-    system("cls");
+typedef struct estruturaProcesso {
+    pid_t pid;
+    int codigoStatusExecucao;
+} estruturaProcesso;
 
-    show_menu();
-    int lv_option = get_option();
-    show_menu();
+//Funções do menu de seleção
+int menuSelecao();
+void executaOpcao(int opcao, estruturaProcesso processo[]);
+void executaOpcaoProcessoFilho(int opcao);
+void executaOpcaoProcessoPai(int opcao);
 
+//Funcionalidades dos processos filhos
+void webBrowser();
+void textEditor();
+void terminal();
 
+//Funcionalidades do processo pai
+void registraInformacaoProcessoFilho();
+void finalizarProcesso();
+void quit();
+
+int main(){
+    estruturaProcesso processo[3];
+    while(1){
+        executaOpcao(menuSelecao(), processo);
+    }
+}
+
+int menuSelecao(){
+    int opcao = 0;
+
+    printf(" <<<< Applications Menu >>>\n");
+    printf("\t1) Web Browser\n");
+    printf("\t2) Text Editor\n");
+    printf("\t3) Terminal\n");
+    printf("\t4) Finalizar processo\n");
+    printf("\t5) Quit\n");
+    printf(" Opção: ");
+    scanf("%d", &opcao);
+    return opcao;
+}
+
+void executaOpcao(int opcao, estruturaProcesso processo[]){
+    if ((processo[opcao].pid=fork()) < 0) {
+        perror("Erro no fork!");
+    } else if (processo[opcao].pid == 0) {
+        executaOpcaoProcessoFilho(opcao);
+    } else {
+        //Se é uma funcionalidade executada no processo pai
+        if (opcao == FINALIZAR_PROCESSO || opcao == QUIT){
+            executaOpcaoProcessoPai(opcao);
+        } else {
+            registraInformacaoProcessoFilho();
+        //colocar aqui a parte que ira registrar o pid e o status do processo que ocorreu no filho
+        }
+    }
+}
+
+void executaOpcaoProcessoFilho(int opcao){
+    switch (opcao) {
+        case WEB_BROWSER
+            webBrowser();
+        break;
+        case TEXT_EDITOR:
+            textEditor();
+        break;
+        case TERMINAL:
+            terminal();
+        break;
+    }
+}
+
+void webBrowser(){
+    char url[200];
+
+    printf("Digite a URL que deseja buscar: ");
+    scanf("%s", &url);
+    
+}
+
+void textEditor(){
+    printf("TE");
+}
+
+void terminal(){
+    printf("T");
+}
+
+void executaOpcaoProcessoPai(int opcao){
+    switch (opcao) {
+        case FINALIZAR_PROCESSO:
+            finalizarProcesso();
+        break;
+        case QUIT:
+            quit();
+        break;
+    }
+}
+
+void registraInformacaoProcessoFilho(){
+    printf("Registrar aqui");
+}
+
+void finalizarProcesso(){
+    printf("FP");
+}
+
+void quit(){
+    printf("Q");
 }
