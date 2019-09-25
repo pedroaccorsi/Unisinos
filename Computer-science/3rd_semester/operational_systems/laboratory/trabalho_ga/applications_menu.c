bdb4f047a398a074b48c5e55d0166e4cfd706159
Accorci <pedrooacorsi@gmail.com>
2019-09-25 16:56:40 -0300

    Update applications_menu.c

--- Computer-science/3rd_semester/operational_systems/laboratory/trabalho_ga/applications_menu.c
+++ Computer-science/3rd_semester/operational_systems/laboratory/trabalho_ga/applications_menu.c
@@ -6,13 +6,15 @@
 #include <signal.h>
 #include <sys/signal.h>
 #include <stdlib.h>
-
+ 
 //Definições de menu
-#define WEB_BROWSER         1
-#define TEXT_EDITOR         2
-#define TERMINAL            3
-#define FINALIZAR_PROCESSO  4
-#define QUIT                5
+enum menu{
+    WEB_BROWSER,
+    TEXT_EDITOR,
+    TERMINAL,
+    FINALIZAR_PROCESSO,
+    QUIT
+};
 
 typedef struct estruturaProcesso {
     int name;
@@ -20,48 +22,27 @@ typedef struct estruturaProcesso {
     char codigoStatusExecucao[32];
 } estruturaProcesso;
 
-estruturaProcesso processos[3]={};
+volatile estruturaProcesso processos[3]={};
 
-void signal_handler();
+int fechou;
 
-//Funções do menu de seleção
-int menuSelecao();
-void executaOpcao(int opcao, estruturaProcesso processo[]);
-void executaOpcaoProcessoFilho(int opcao);
-void executaOpcaoProcessoPai(int opcao);
+#include "func_signatures.h"
 
-//Funcionalidades dos processos filhos
-void webBrowser();
-void textEditor();
-void terminal();
-
-//Funcionalidades do processo pai
-void finalizarProcesso();
-void quit();
-void define_processes();
-void define_sig_handler();
-void sig_handler(int signal);
+int child;
 
 int main(){
 
     define_processes();
-    define_sig_handler();
+    //define_sig_handler();
+
+    signal(SIGTERM, sig_handler);
+    signal(SIGINT , sig_handler);
+    signal(SIGCHLD, sig_handler);
 
     while(1){
         executaOpcao(menuSelecao(), processos);
     }
 
-}
-
-void define_sig_handler(){
-    struct sigaction sa;
-    sa.sa_handler = &sig_handler;
-    sa.sa_flags = SA_RESTART;
-    sigfillset(&sa.sa_mask);
-}
-
-void sig_handler(int signal){
-    exit(15);
 }
 
 void define_processes(){
@@ -77,12 +58,43 @@ void define_processes(){
 
 }
 
+void define_sig_handler(){
+    struct sigaction action;
+	memset(&action, 0, sizeof(struct sigaction));
+	action.sa_handler = sig_handler;
+	sigaction(SIGTERM, &action, NULL);
+}
+
+void sig_handler(int signal){
+
+    switch (signal) {
+
+        case SIGCHLD:
+            child += 1;
+             break;
+        
+        case SIGTERM:
+            if (getpid() == 0){
+                 exit(15);
+            }
+                
+            break;
+
+        case SIGINT:
+             break;
+    }
+
+}
+
+
 int menuSelecao(){
     
     int opcao = 0;
 
     system("clear"); 
-    printf(" <<<< Applications Menu >>>\n");
+    printf("fechou -> %d\n", fechou);
+
+    printf(" \n<<<< Applications Menu >>>\n");
     printf("\t1) Web Browser             (%s, pid = %d)\n", processos[0].codigoStatusExecucao, processos[0].pid);
     printf("\t2) Text Editor             (%s, pid = %d)\n", processos[1].codigoStatusExecucao, processos[1].pid);
     printf("\t3) Terminal                (%s, pid = %d)\n", processos[2].codigoStatusExecucao, processos[2].pid);
@@ -121,57 +133,65 @@ void webBrowser(){
 
     char url[200];
     int status;
+    int new_pid;
     
     printf("Digite a URL que deseja buscar: "); 
     scanf("%s", &url);
 
-    strcpy(processos[0].codigoStatusExecucao, "running");
-    processos[0].name = WEB_BROWSER;
-    processos[0].pid  = fork();     
+    new_pid = fork();     
 
-    switch (processos[0].pid){
+    switch (new_pid){
 
         case -1:
             strcpy(processos[0].codigoStatusExecucao, "failed");
-            printf("deu ruim");
             break;
         
         case 0:
             
-            execlp("firefox","firefox --new-window", url, NULL); 
+            execlp("firefox","firefox --new-window", url, NULL);     
+            exit(13);
             break;
+
+        default:
+            strcpy(processos[0].codigoStatusExecucao, "running");
+            processos[0].name = WEB_BROWSER;
+            processos[0].pid = new_pid;
+            break;
+
     } 
     
 }
 
 void textEditor(){
 
-    strcpy(processos[1].codigoStatusExecucao, "running");
-    processos[1].name = TEXT_EDITOR;
-    processos[1].pid  = fork();     
+    int new_pid = fork();     
 
-    switch (processos[1].pid){
+    switch (new_pid){
 
         case -1:
             strcpy(processos[1].codigoStatusExecucao, "failed");
-            printf("deu ruim");
             break;
         
         case 0:
-            
+
             execlp("gedit","gedit --new-window", NULL); 
             break;
+
+        default:
+            strcpy(processos[1].codigoStatusExecucao, "running");
+            processos[1].name = TEXT_EDITOR;
+            processos[1].pid  = new_pid;
+            break;
+
     } 
 
 }
 
 void terminal(){
 
-    strcpy(processos[2].codigoStatusExecucao, "running");
-    processos[2].name = TERMINAL;
-    processos[2].pid  = fork();     
+    int new_pid = fork();     
 
-    switch (processos[2].pid){
+    switch (new_pid){
 
         case -1:
             strcpy(processos[2].codigoStatusExecucao, "failed");
@@ -180,6 +200,12 @@ void terminal(){
         
         case 0:
             execl("/usr/bin/bash", "bash", "-c", "xs", NULL);
+            break;
+
+        default:
+            strcpy(processos[2].codigoStatusExecucao, "running");
+            processos[2].name = TERMINAL;
+            processos[2].pid  = new_pid;
             break;
     } 
 
@@ -210,7 +236,7 @@ void finalizarProcesso(){
         
         strcpy(processos[0].codigoStatusExecucao, "aborted");
         processos[0].pid = 0;
-        kill(processos[0].pid, SIGTERM);
+        fechou = kill(processos[0].pid, SIGTERM);
         break;
 
     case TEXT_EDITOR:
